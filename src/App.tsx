d326b28b3295c8d23e3e97fba6c457d5039a49c2
nabilalaa <nabilalaa06@gmail.com>
2025-11-09 09:54:11 +0200

    booking

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,10 @@
 import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/ui/navBar";
-// import About from "./components/About";
-import ContactUs from "./pages/ContactUs/ContactUs.tsx";
-import Booking from "./pages/Booking/Booking.tsx";
-import Privacy from "./pages/Privacy/Privacy.tsx";
 
-import Footer from "./components/ui/footer.tsx";
-import SearchDoctor from "./pages/SearchDoctor/SearchDoctor.tsx";
+// import SearchDoctor from "./pages/SearchDoctor/SearchDoctor.tsx";
 
-function App() {
-    return (
-        <>
-            <NavBar />
-            <Routes>
-                <Route path="/contact" element={<ContactUs />} />
-                {/* <Route path="/" element={<Home />} /> dont forget import the component */}
-                <Route path="/booking" element={<Booking />} />
-                <Route path="/privacy" element={<Privacy />} />
-                <Route path="/Search-Doctor" element={<SearchDoctor />} />
-            </Routes>
-            {/* <SignIn /> */}
-            <Footer />
-        </>
-    );
+
 import Footer from "./components/ui/footer";
 import ContactUs from "./pages/ContactUs/ContactUs";
 import Booking from "./pages/Booking/Booking";
